afc3d274d2672721287c7f4f1747a43d7c216345
kwkaess <kwkaess@gmx.com>
2017-02-17 22:28:15 -0600

    Final initial layout. Will add more as other code requires.

--- regexeditor.cpp
+++ regexeditor.cpp
@@ -6,6 +6,9 @@ RegExEditor::RegExEditor(QWidget *parent) :
     ui(new Ui::RegExEditor)
 {
     ui->setupUi(this);
+    ui->tableWidget->setColumnWidth(0,50);
+   // ui->tableWidget->setColumnWidth(1,250);
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(1,QHeaderView::Stretch);
 }
 
 RegExEditor::~RegExEditor()
